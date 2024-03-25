5bdc48a7d4be405f71a957aad05999927063ca9e
Thamsanqa <22351156@live.mut.ac.za>
2024-03-25 09:30:03 +0200

    Resolve merge conflict in feature-voter-registration

---
+++ tp2_test1/registration.py
@@ -0,0 +1,5 @@
+""" make changes by adding a python
+file that displays a message “Register to vote, your vote is your power to make a change”
+named “registration.py”, and merge it back to the main branch. """
+
+print("Register to vote, your vote is your power to make a change")
